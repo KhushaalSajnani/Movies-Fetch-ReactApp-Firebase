ef196948c22b3295f366a4ece072b45371f3f6a2
khushaalSajnani <khushaalsajnani@gmail.com>
2023-01-10 21:10:47 +0530

    Fetch Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import React, {useEffect, useState, useCallback} from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
+const URL = 'https://swapi.dev/api/films';
 function App() {
+  const[movies, setMovies] = useState([]);
+  const[isLoading,setIsLoading] = useState(false);
+  const[ifError,setIfError] = useState();
+
+
+   //  OLD WAY
+    {/*
+  const fetchHandler = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch(URL)
+        .then((res) => {
+            return res.json();
+        })
+        .then(receivedData => {
+            // console.log(receivedData.results)
+            const updatedData = receivedData.results.map((movie) => {
+                return {
+                    id: movie.episode_id,
+                    title:movie.title,
+                    openingText:movie.opening_crawl,
+                    releasingDate:movie.release_date
+                }
+            })
+            setMovies(updatedData);
+            setIsLoading(false);
+        })
+  }; */}
+
+   // MODERN WAY
+   const modernFetchHandler = useCallback(async() => {
+       setIfError(null);
+       setIsLoading(true)
+       try {
+           const response = await fetch(URL);
+           const receivedData = await response.json();
+
+           if(!response.ok){
+               throw new Error("ERROR IN FETCH!")
+           }
+
+
+           const updatedData = receivedData.results.map((movie) => {
+               return {
+                   id: movie.episode_id,
+                   title: movie.title,
+                   openingText: movie.opening_crawl,
+                   releasingDate: movie.release_date
+               }
+           })
+           setMovies(updatedData);
+       }catch (e){
+            setIfError(e.message)
+       }finally {
+           setIsLoading(false)
+       }
+   },[])
+    useEffect(()=>{
+        modernFetchHandler();
+    },[modernFetchHandler])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={modernFetchHandler}>Fetch Movies</button>
+      </section>
+      <section>
+          {!isLoading && movies.length>0 && <MoviesList movies={movies} />}
+          {!isLoading && movies.length===0 && <p>NO MOVIE FOUND IN DB !</p>}
+          {!isLoading && ifError && <p>{ifError}</p>}
+          {isLoading && <p>LOADING</p>}
+      </section>
+    </React.Fragment>
   );
 }
 
